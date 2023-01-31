3da6d549c6b216d8cab56a760f6933dc2443710e
dylaneamos <122559195+dylaneamos@users.noreply.github.com>
2023-01-31 12:51:11 +0300

    Add files via upload

---
+++ smarttransformer.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+
+enum Issue {
+  NO_ISSUES,
+  OVERCURRENT,
+  OVERVOLTAGE,
+  HIGH_TEMPERATURE,
+  HIGH_PRESSURE
+};
+
+const double threshold_current = 10.0;
+const double threshold_voltage = 120.0;
+const double threshold_temperature = 60.0;
+const double threshold_pressure = 50.0;
+
+Issue analyze_data(double current, double voltage, double temperature, double pressure) {
+  // Analyze the data and detect potential issues
+  if (current > threshold_current) {
+    return OVERCURRENT;
+  }
+  if (voltage > threshold_voltage) {
+    return OVERVOLTAGE;
+  }
+  if (temperature > threshold_temperature) {
+    return HIGH_TEMPERATURE;
+  }
+  if (pressure > threshold_pressure) {
+    return HIGH_PRESSURE;
+  }
+  return NO_ISSUES;
+}
+
+void trigger_action(Issue issue) {
+  // Take action based on the detected issue
+  switch (issue) {
+    case OVERCURRENT:
+      std::cout << "Overcurrent detected, isolating the fault." << std::endl;
+      break;
+    case OVERVOLTAGE:
+      std::cout << "Overvoltage detected, isolating the fault." << std::endl;
+      break;
+    case HIGH_TEMPERATURE:
+      std::cout << "High temperature detected, providing diagnostic information." << std::endl;
+      break;
+    case HIGH_PRESSURE:
+      std::cout << "High pressure detected, providing diagnostic information." << std::endl;
+      break;
+    case NO_ISSUES:
+      std::cout << "No issues detected." << std::endl;
+      break;
+  }
+}
+
+double read_current_sensor() {
+  return 1000.0;
+}
+
+double read_voltage_sensor() {
+  return 100.0;
+}
+
+double read_temperature_sensor() {
+  return 50.0;
+}
+
+double read_pressure_sensor() {
+  return 40.0;
+}
+
+int main() {
+	
+  // Main loop to collect data and trigger actions
+  while (true) {
+    double current = read_current_sensor();
+    double voltage = read_voltage_sensor();
+    double temperature = read_temperature_sensor();
+    double pressure = read_pressure_sensor();
+
+    Issue issue = analyze_data(current, voltage, temperature, pressure);
+    trigger_action(issue);
+
+    std::this_thread::sleep_for(std::chrono::seconds(5)); // wait 5 seconds before collecting data again
+  }
+  return 0;
+}
